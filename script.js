c9563264fc64a460a3f150ddf5c9dd74ae1bd8b6
Ryan Chavungama <chavungamatadiwa@gmail.com>
2022-06-27 22:35:39 +0200

    layout has been completed....now working on rating logic.

--- script.js
+++ script.js
@@ -16,22 +16,71 @@ let initialState = document.querySelector('#initial_state');
 
 let card = document.querySelector('#card');
 
-function ratingValue (ratingButton){
-    let rating = ratingButton.textContent;
-}
+let numOne = ratingButtonOne.addEventListener('click', evt => {
 
-ratingButtonOne.addEventListener('click', ratingValue(ratingButtonOne));
+    let rating = evt.target.textContent;
+    console.log(rating);
 
-ratingButtonTwo.addEventListener('click', ratingValue(ratingButtonTwo));
+    return Int(rating);
 
-ratingButtonThree.addEventListener('click', ratingValue(ratingButtonThree));
 
-ratingButtonFour.addEventListener('click', ratingValue(ratingButtonFour));
+} );
 
-ratingButtonFive.addEventListener('click', ratingValue(ratingButtonFive));
+let numTwo = ratingButtonTwo.addEventListener('click', evt => {
+    
+    let rating = evt.target.textContent;
+    console.log(rating);
+
+    return Int(rating);
+
+});
+
+let numThree = ratingButtonThree.addEventListener('click', evt=> {
+    
+    let rating = evt.target.textContent;
+    console.log(rating);
+
+    return Int(rating);
+
+});
+
+let numFour = ratingButtonFour.addEventListener('click', evt =>{
+
+    let rating = evt.target.textContent;
+    console.log(rating);
+
+    return Int(rating);
+
+});
+
+let numFive = ratingButtonFive.addEventListener('click', evt=>{
+
+    let rating = evt.target.textContent;
+    console.log(rating)
+
+    return Int(rating);
+
+
+});
 
 submitButton.addEventListener('click', ()=>{
-    
+   /* RATING LOGIC....WELL SORTA😅😅
+    let rating_num;
+
+    if (numOne > 0){
+        return rating_num += numOne;
+    } elseif (numTwo > 0){
+        return rating_num += numTwo;
+    } elseif (numThree){
+
+    } elseif(numFour){
+
+    } elseif (numFive){
+
+    } else{
+        return console.log('ERROR');
+    }
+    */
     initialState.remove();
 
     let thankYouState = document.createElement('div');
@@ -44,9 +93,34 @@ submitButton.addEventListener('click', ()=>{
 
     image.setAttribute('id', 'thank_you')
 
-    image.src = 'illustration-thank-you.svg';
+    image.src = 'images/illustration-thank-you.svg';
 
     thankYouState.appendChild(image);
+
+    let thanksHeading = document.createElement('h1');
+
+    thanksHeading.setAttribute('id', 'thanks_heading')
+
+
+    let yourRating = document.createElement('p')
+
+    yourRating.setAttribute('id', 'your_rating');
+
+    let thankYouText = document.createElement('p');
+
+    thankYouText.setAttribute('id', 'thank_you_text');
+
+    thankYouState.appendChild(yourRating);
+
+    thankYouState.appendChild(thanksHeading);
+
+    thankYouState.appendChild(thankYouText);
+
+    yourRating.textContent = 'You selected ' + '*****' + ' out of 5';
+
+    thanksHeading.textContent = 'Thank you!';
+
+    thankYouText.textContent = ' We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!';
 
 
 });
